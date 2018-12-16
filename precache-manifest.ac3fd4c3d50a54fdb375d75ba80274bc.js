4ab4741e738c9735ea89c3235fb33878b82c1844
查sir <760654225@qq.com>
2018-12-16 18:38:36 +0800

    tranlate

---
+++ precache-manifest.ac3fd4c3d50a54fdb375d75ba80274bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "9d0c9f404f3d295ca3939323cbbf052e",
+    "url": "index.html"
+  },
+  {
+    "revision": "73e866dfec1fc6c0b97c",
+    "url": "assets/js/index.30e16d26.js"
+  },
+  {
+    "revision": "dcd8f80944e59e2c9f1f",
+    "url": "assets/js/chunk-vendors.580333f3.js"
+  },
+  {
+    "revision": "73e866dfec1fc6c0b97c",
+    "url": "assets/css/index.eb61f59f.css"
+  }
+];
